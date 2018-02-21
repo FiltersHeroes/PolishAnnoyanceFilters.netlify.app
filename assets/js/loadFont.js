d505699e4853203d4793da59f1ce248704da10dc
hawkeye116477 <hawkeye116477@gmail.com>
2018-02-21 13:57:58 +0100

    Revert "Load font async"
    This reverts commit 3e20be1ae8aa74296d0494bcd0db37d3226f1bf5.

--- assets/js/loadFont.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    var fonts = 'https://fontlibrary.org/face/architect-s-daughter';
-    if (document.createStyleSheet) document.createStyleSheet(fonts);
-    else $("head").append($("<link rel='stylesheet' href='"+ fonts +"' type='text/css' media='screen' />"));
-});
