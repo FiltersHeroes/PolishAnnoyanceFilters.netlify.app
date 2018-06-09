58e47dab1269b8b44f019278d167748a34700b77
hawkeye116477 <hawkeye116477@gmail.com>
2018-06-10 00:05:51 +0200

    Update freeDays script

--- assets/js/freeDays.js
+++ assets/js/freeDays.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var date = new Date();
     var weekOfMonth = (0 | date.getDate() / 7)+1;
-    if (date.getDay()==0 && weekOfMonth==1 || date.getDay()==0 && weekOfMonth==2 || date.getDay()==0 && weekOfMonth==4)
+    if (date.getDay()==0 && weekOfMonth==1 || date.getDay()==0 && weekOfMonth==3 || date.getDay()==0 && weekOfMonth==4)
     {
         alert("Dzisiaj mamy wolne :-)");
         $("#content-wrapper").remove();
