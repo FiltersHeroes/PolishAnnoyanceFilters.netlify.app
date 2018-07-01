6244e114637f43c2966c3e01b77e1dc59366a058
hawkeye116477 <hawkeye116477@gmail.com>
2018-07-01 12:14:24 +0200

    Update page [ci skip]

--- assets/js/freeDays.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var date = new Date();
-    var weekOfMonth = (0 | date.getDate() / 7)+1;
-    if (date.getDay()==0 && weekOfMonth==1 || date.getDay()==0 && weekOfMonth==3 || date.getDay()==0 && weekOfMonth==4)
-    {
-        alert("Dzisiaj mamy wolne :-)");
-        $("#content-wrapper").remove();
-    }
-});
