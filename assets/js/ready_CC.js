1bbd0c0f4c923ba112845028fc275165cf98df91
hawkeye116477 <hawkeye116477@gmail.com>
2018-11-22 16:13:50 +0100

    Fix Cookie form

--- assets/js/ready_CC.js
+++ assets/js/ready_CC.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
     autosize($('textarea'));
-    $('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSfFbscacdIgmavjVXGBTl9wPYaxSZ41YDIdp9oTzeEZId0a9A/formResponse');
+    $('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSfMGeBhePTjTP9YOeuKo0YgbOrOg9l6x_5XTV47kLBbOPN1wQ/formResponse');
 });
