fd4446875b16b66dbbc0de92354b5b47cc6d5a04
hawkeye116477 <hawkeye116477@gmail.com>
2018-11-22 16:56:31 +0100

    Fix form

--- assets/js/ready_CC.js
+++ assets/js/ready_CC.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
     autosize($('textarea'));
+    $('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSfFbscacdIgmavjVXGBTl9wPYaxSZ41YDIdp9oTzeEZId0a9A/formResponse');
 });
