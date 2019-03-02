2637cb94935851392273c3e74f4e8ec6f28aa5c6
hawkeye116477 <hawkeye116477@gmail.com>
2019-03-02 11:54:23 +0100

    Poprawki

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    autosize($('textarea'));
-    $('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSfMGeBhePTjTP9YOeuKo0YgbOrOg9l6x_5XTV47kLBbOPN1wQ/formResponse');
+	autosize($('textarea'));
+	$('#usrform').attr('action', 'https://docs.google.com/forms/d/e/1FAIpQLSfMGeBhePTjTP9YOeuKo0YgbOrOg9l6x_5XTV47kLBbOPN1wQ/formResponse');
 });
 
 function checkAdblocker(){
 	var annoyance = document.querySelector("#pafdetect");
-    var pafadguard = document.querySelector('#pafadguard');
+	var pafadguard = document.querySelector('#pafadguard');
 
-	if((typeof(pafdetect) != 'undefined') && pafdetect === true)
+	if((typeof(pafDetect) != 'undefined') && pafDetect === true)
 	{
 		Swal.fire("Wspaniale!", "Masz aktywny uBlock Origin lub Nano Adblocker z Polskimi Filtrami Elementów Irytujących 😊", "success");
 	}
@@ -21,7 +21,7 @@ function checkAdblocker(){
 			title: 'Dobrze, choć mogłoby być lepiej!',
 			type: 'warning',
 			html: 'Prawdopodobnie masz aktywny AdBlock/AdBlock Plus z Polskimi Filtrami Elementów Irytujących. Zalecamy zamienić go na <a href="https://github.com/gorhill/uBlock#installation" target="_blank" rel="noopener">uBlock Origin</a>, by zniknęło więcej elementów irytujących.'
-		  })
+		})
 	}
 	else
 	{
@@ -29,7 +29,7 @@ function checkAdblocker(){
 			title: 'Bardzo źle!',
 			type: 'error',
 			text: 'Nie masz aktywnych Polskich Filtrów Elementów Irytujących 😞',
-			confirmButtonText: '<a href="abp:subscribe?location=https%3A%2F%2Fraw.githubusercontent.com%2FPolishFiltersTeam%2FPolishAnnoyanceFilters%2Fmaster%2FPPB.txt&amp;title=Polskie%20Filtry%20Element%C3%B3w%20Irytuj%C4%85cych">Subskrybuj Polskie Filtry Elementów Irytujących</a>',
-		  })
+		})
+		$("button.swal2-confirm").replaceWith('<a class="swal2-confirm swal2-styled" style="border-left-color: rgb(48, 133, 214); border-right-color: rgb(48, 133, 214);" href="abp:subscribe?location=https%3A%2F%2Fraw.githubusercontent.com%2FPolishFiltersTeam%2FPolishAnnoyanceFilters%2Fmaster%2FPPB.txt&amp;title=Polskie%20Filtry%20Element%C3%B3w%20Irytuj%C4%85cych">Subskrybuj Polskie Filtry Elementów Irytujących</a>');
 	}
 }
