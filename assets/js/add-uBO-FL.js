c03769c3f428a4b0356d48c9e041aff176b3cb5a
hawkeye116477 <hawkeye116477@gmail.com>
2020-12-29 23:36:45 +0100

    Aktualizacja BS, swal 2 i poprawka okienka manualnej subskrypcji

--- assets/js/add-uBO-FL.js
+++ assets/js/add-uBO-FL.js
@@ -1,61 +1,22 @@
 function createuBOModal(url) {
-    if (document.getElementById("uBOmodal") == null) {
-        var modal = document.createElement('div');
-        modal.setAttribute('class', 'modal fade');
-        modal.setAttribute('tabindex', "-1");
-        modal.setAttribute('role', 'dialog');
-        modal.setAttribute('id', 'uBOmodal');
-        modal.setAttribute('aria-labelledby', 'myModalLabel');
-        var mainContent = document.getElementById("main-content");
-        mainContent.appendChild(modal);
-        var modalDialog = document.createElement('div');
-        modalDialog.setAttribute('class', 'modal-dialog modal-vlg');
-        modalDialog.setAttribute('role', 'document');
-        modal.appendChild(modalDialog);
-        var modalContent = document.createElement('div');
-        modalContent.setAttribute('class', 'modal-content');
-        modalDialog.appendChild(modalContent);
-        var modalHeader = document.createElement('div');
-        modalHeader.setAttribute('class', 'modal-header');
-        modalContent.appendChild(modalHeader);
-        var title = document.createElement('h4');
-        title.setAttribute('class', 'modal-title');
-        title.setAttribute('id', 'myModalLabel');
-        title.textContent = "Ręczna subskrybcja listy filtrów";
-        modalHeader.appendChild(title);
-        var smallCloseBtn = document.createElement('button');
-        smallCloseBtn.setAttribute('type', 'button');
-        smallCloseBtn.setAttribute('class', 'close');
-        smallCloseBtn.setAttribute('data-dismiss', 'modal');
-        smallCloseBtn.setAttribute('aria-label', 'Close');
-        modalHeader.appendChild(smallCloseBtn);
-        var span = document.createElement('span');
-        span.setAttribute('aria-hidden', 'true');
-        smallCloseBtn.appendChild(span);
-        var x = document.createTextNode("x");
-        smallCloseBtn.appendChild(x);
-        var modalBody = document.createElement('div');
-        modalBody.setAttribute('class', 'modal-body');
-        modalContent.appendChild(modalBody);
-        modalBody.innerHTML = '<p>W miejsce 3 wklej adres z listą filtrów </p>'+'<code ><input type="text" id="manualSubscribe" readonly="true" value="'+url+'"</input></code><button class="btn btn-light" style="margin-left: 10px;" onclick="copyToClipBoard();"><i class="icon-copy" title="kopiuj link"></i></button><pre><img src="/images/uBO_add1.png" alt="ubo" title="klikamy w przycisk ustawień"><img src="/images/uBO_add2.png" alt="ubo-settings" title="aktywujemy zakładkę listy filtrów, przewijamy stronę do końca i wklejamy link z listą do pola na dole, potem klikamy zastosuj"></pre>';
-        var modalFooter = document.createElement('div');
-        modalFooter.setAttribute('class', 'modal-footer');
-        modalContent.appendChild(modalFooter);
-        var closeBtn = document.createElement('button');
-        closeBtn.setAttribute('type', 'button');
-        closeBtn.setAttribute('class', 'btn btn-default');
-        closeBtn.setAttribute('data-dismiss', 'modal')
-        var closeText = document.createTextNode("Zamknij");
-        closeBtn.appendChild(closeText);
-        modalFooter.appendChild(closeBtn);
-    }
-
-    $('#uBOmodal').modal('show');
-
-    $('#uBOmodal').on('hidden.bs.modal', function () {
-        $('#uBOmodal').remove();
-    })
-
+        Swal.fire({
+            title: 'Ręczna subskrypcja listy filtrów',
+            html:
+            '<p>Jeśli z jakiegoś powodu nie wyświetlił ci się komunikat z możliwością potwierdzenia subskrypcji, to w miejsce nr 3 wklej adres z listą filtrów.</p><input type="text" id="manualSubscribe" class="form-control" readonly="true" value="'+url+'"><button class="btn btn-light" style="margin-left: 10px;" onclick="copyToClipBoard();" title="kopiuj link">'+
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16"><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg>'+
+            '</button><div class="mt-3"><img class="img-fluid" src="/images/uBO_add1.png" alt="ubo" title="klikamy w przycisk ustawień"><img class="img-fluid" src="/images/uBO_add2.png" alt="ubo-settings" title="aktywujemy zakładkę listy filtrów, przewijamy stronę do końca i wklejamy link z listą do pola na dole, potem klikamy zastosuj"></div>',
+            showCloseButton: true,
+            showCancelButton: false,
+            showDenyButton: true,
+            focusConfirm: true,
+            grow: 'row',
+            confirmButtonText: "Zamknij",
+            denyButtonText: "Nie pokazuj więcej tych instrukcji",
+        }).then((result) => {
+            if (result.isDenied) {
+                localStorage.setItem('showManualSubscribe', 'false')
+            }
+        })
 }
 
 var pafDetect = true;
